fa42badda87d4d5ff4788d461816ec5fa33ee70b
AminFarzinrad <a.farzinrad062@gmail.com>
2024-11-29 09:47:31 +0330

    generating a ramdom dice roll and display dice

--- js/app.js
+++ js/app.js
@@ -3,7 +3,17 @@
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
-// starting project 
+const btnRoll = document.querySelector('.btn--roll');
+// starting project
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+// rolling dice
+btnRoll.addEventListener('click', function () {
+  // 1. generating a ramdom dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `./image/dice-${dice}.png`;
+  // 3. check for rolled 1 : 1 if true , switch to next player
+});
