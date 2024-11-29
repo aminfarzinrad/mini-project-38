03480bf4695578a4965e558bbb369ff3a68e0238
AminFarzinrad <a.farzinrad062@gmail.com>
2024-11-29 11:04:58 +0330

    Making a reset button

--- js/app.js
+++ js/app.js
@@ -9,13 +9,14 @@ const current1El = document.getElementById('current--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
 // starting project
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 let currentScore = 0;
 let activePlayer = 0;
-const scores = [0, 0];
+let scores = [0, 0];
 let playing = true;
 // function
 const switchPlayer = function () {
@@ -54,7 +55,8 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. check if player's score is >= 100
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 10) {
+      //FIXME
       // finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -69,4 +71,19 @@ btnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
+});
+// btn reset
+btnNew.addEventListener('click', function () {
+  playing = true;
+  scores = [0, 0];
+  activePlayer = 0;
+  currentScore = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
 });
