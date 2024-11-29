29a6d1b9fc13b2b01b2061dc44403ceabd6e92d8
AminFarzinrad <a.farzinrad062@gmail.com>
2024-11-29 10:24:37 +0330

    When holding, the current score is added to the total score and then the player changes

--- js/app.js
+++ js/app.js
@@ -8,12 +8,14 @@ const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
+const btnHold = document.querySelector('.btn--hold');
 // starting project
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 let currentScore = 0;
 let activePlayer = 0;
+const scores = [0, 0];
 // rolling dice
 btnRoll.addEventListener('click', function () {
   // 1. generating a ramdom dice roll
@@ -36,4 +38,21 @@ btnRoll.addEventListener('click', function () {
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
   }
+});
+// btn hold
+btnHold.addEventListener('click', function () {
+  // 1. add currentScore to active player's score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  // 2. check if player's score is >= 100
+  // finish the game
+  
+  // 3. switch player
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
 });
