251adc7d60f15de15b86438a59e541ffd629c9e7
AminFarzinrad <a.farzinrad062@gmail.com>
2024-11-29 11:12:51 +0330

    Clean up the code for the initial website page load and the reset button

--- js/app.js
+++ js/app.js
@@ -10,15 +10,26 @@ const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const btnHold = document.querySelector('.btn--hold');
 const btnNew = document.querySelector('.btn--new');
-// starting project
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-let currentScore = 0;
-let activePlayer = 0;
-let scores = [0, 0];
-let playing = true;
-// function
+// starting project // functions
+let scores, currentScore, activePlayer, playing;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 const switchPlayer = function () {
   currentScore = 0;
   document.getElementById(`current--${activePlayer}`).textContent =
@@ -55,8 +66,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. check if player's score is >= 100
-    if (scores[activePlayer] >= 10) {
-      //FIXME
+    if (scores[activePlayer] >= 100) {
       // finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -73,17 +83,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 // btn reset
-btnNew.addEventListener('click', function () {
-  playing = true;
-  scores = [0, 0];
-  activePlayer = 0;
-  currentScore = 0;
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-});
+btnNew.addEventListener('click', init);
